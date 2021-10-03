e22d13867681066e8f8d3fe79b0f252cf3de0a0e
jessicafeese <jaf32246@creighton.edu>
2021-10-03 13:51:59 -0500

    update

---
+++ forms/functionCompare/functionCompare.js
@@ -0,0 +1,16 @@
+/*
+function Compare(pass1, pass2) {
+  password = pass1
+  confirm = pass2
+  let passCompare = password.localeCompare(confirm)
+  if (passCompare == 0) {
+    console.log('The passwords are the same.')
+  }else{
+    console.log('The passwords are not the same.')
+  }
+}
+
+let password1 = prompt('Enter your password: ')
+let password2 = prompt('Confirm your password: ')
+let comparePass = Compare(password1, password2)
+*/
