26ef0d966fed651ed5242314f235cc70f62d3d95
jessicafeese <jaf32246@creighton.edu>
2021-10-03 12:17:35 -0500

    functionCompare

---
+++ forms/cityWhile/cityWhile.js
@@ -0,0 +1,22 @@
+/*
+let wantToContinue = true
+let cities = []
+
+while (wantToContinue) {
+  if (cities == '') {
+    newCity = prompt("Enter a city: ")
+    cities.push(newCity)
+    wantToContinue = confirm('Do you want to enter another city? OK for Yes, Cancel for No.')
+  } else {
+    newCity = prompt("Enter another city: ")
+    cities.push(newCity)
+    wantToContinue = confirm('Do you want to enter another city? OK for Yes, Cancel for No.')
+  }
+}
+
+let i = 0
+while (i < cities.length) {
+  console.log(cities[i].toLowerCase())
+  i++;
+}
+*/
