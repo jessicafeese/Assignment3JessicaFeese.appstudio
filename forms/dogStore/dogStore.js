eaeb996fe1ac1aa1710de67c854c54eca8a2e542
jessicafeese <jaf32246@creighton.edu>
2021-10-03 22:20:37 -0500

    Final

--- forms/dogStore/dogStore.js
+++ forms/dogStore/dogStore.js
@@ -1,14 +1,14 @@
-let products = ["Crocheted Dog Toy $10.00", "Slow Feeder Bowl $14.00", "Pawz Grey Treat Jar $20.00", "Blue Sweet Dreams Thermal Pajamas $30.00", "Checker Chewy Vuiton Bowl $32.00", "Red Heart Sweater Dress $27.00", "Velvet Hair Bow - Sun Kissed $12.00", "Into the Wild Leash $27.00"]
+
+let products = ["Crocheted Dog Toy $10.00","Slow Feeder Bowl $14.00", "Pawz Grey Treat Jar $20.00", "Blue Sweet Dreams Thermal Pajamas $30.00","Checker Chewy Vuiton Bowl $32.00", "Red Heart Sweater Dress $27.00", "Velvet Hair Bow - Sun Kissed $12.00", "Into the Wild Leash $27.00"]
 let wishlist = []
 
 
 btnProducts.onclick=function(){
-  let display = lblDisplayProducts.value
   lblDisplayProducts.value = products
 }
 
-btnAddWishList.onclick=function(){
-  let add = inptWishListProduct.value
+btnAdd.onclick=function(){
   wishlist.push(inptWishListProduct.value)
   lblWishList.value = wishlist
 }
+
